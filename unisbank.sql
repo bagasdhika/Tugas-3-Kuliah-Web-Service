f266205664b50e236a7a8b662e5949399de456ff
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2018-03-24 09:01:36 +0700

    Add files via upload

---
+++ unisbank.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2018 at 03:00 AM
+-- Server version: 5.5.32-log
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `unisbank`
+--
+CREATE DATABASE IF NOT EXISTS `unisbank` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `unisbank`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `nim` varchar(30) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `progdi` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `progdi`) VALUES
+('15.01.53.0044', 'Dhika Bagas Whisnu Aji', 'Semarang', 'Teknik Informatika');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
